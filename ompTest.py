0c1791063d3c0a520ead9355963286f06b8d250c
unknown <timdereuse@gmail.com>
2019-04-24 20:49:30 -0400

    it's working on artificial data, at least!

--- ompTest.py
+++ ompTest.py
@@ -13,23 +13,24 @@ sound = sound / max(sound)
 
 sr = 44100
 gauss_eps = 0.001
+resid_eps = 0.001
 
 num_harmonics = 10
-npts = sr * 1
+npts = sr
 sound = sound[:npts]
 domain = range(npts)
 
 u_grid = np.linspace(0, npts, 50)
 u_spacing = u_grid[1] - u_grid[0]
 
-s_grid = np.geomspace(sr / 100, sr / 25, 2)
+s_grid = np.geomspace(u_spacing, u_spacing * 2, 2)
 s_windows = []
 for s in s_grid:
     window = signal.gaussian(npts, s)
     window[window < gauss_eps] = 0
     s_windows.append(window)
 
-f0_grid = np.geomspace(65.406 * 4, 65.406 * 8, 12) * (2 * np.pi / sr)
+f0_grid = np.geomspace(65.406 * 2, 65.406 * 8, 12) * (2 * np.pi / sr)
 
 # define harmonic subspaces
 # project onto each harmonic subspace
@@ -47,7 +48,7 @@ print('creating harmonic subspaces...')
 for i, f in enumerate(f0_grid):
 
     centered_domain = np.array(domain) - (len(domain) // 2)
-    main_signal = [np.cos(f * domain * n) for n in range(1, num_harmonics + 1)]
+    main_signal = [np.cos(f * centered_domain * n) for n in range(1, num_harmonics + 1)]
     unwindowed_subspaces.append(main_signal)
     # for w in shifted_windows:
     #     subspace = [w * sig for sig in main_signal]
@@ -56,11 +57,11 @@ for i, f in enumerate(f0_grid):
 
 def get_subspace(window, unw_ss):
     ss = []
-    for ss in unw_ss:
-        atom = (unw_ss * window)
+    for sig in unw_ss:
+        atom = (sig * window)
         atom = atom / np.linalg.norm(atom)
         ss.append(atom)
-    return ss
+    return np.array(ss)
 
 def Q_corr(subspace, residual):
     result = 0
@@ -69,31 +70,34 @@ def Q_corr(subspace, residual):
     return result
 
 # create artificial data
-def artificial_sound(h_subspaces, num):
+def artificial_sound(windows, uw_subspaces, num):
     y = np.zeros(npts)
-    subspaces_use = np.random.choice(len(h_subspaces), num)
-    for s_ind in subspaces_use:
-        ss = np.array(h_subspaces[s_ind]).T
-        coef = np.zeros(len(ss[0]))
-        coef[np.random.randint(len(coef))] = np.random.uniform(0, 1)
+    windows_use = [np.random.choice(len(windows), num), np.random.choice(len(uw_subspaces), num)]
+    for wind_ind, uw_ind in np.array(windows_use).T:
+        ss = get_subspace(windows[wind_ind], uw_subspaces[uw_ind]).T
+        # ss = np.array(h_subspaces[s_ind]).T
+        coef = np.random.uniform(0, 1, len(ss[0]))
         y = y + np.dot(ss, coef)
     return y
 
-# y = artificial_sound(h_subspaces, 15)
+#y = artificial_sound(shifted_windows, unwindowed_subspaces, 19)
 y = sound
 
 resid = y
 reconstruction = np.zeros(npts)
 found_atoms = []
 subspace_inds = list(iter.product(range(len(shifted_windows)), range(len(unwindowed_subspaces))))
+inds_rejected = np.zeros(len(subspace_inds))
 
-for it in range(5):
+for it in range(20):
 
     scores = np.zeros(len(subspace_inds))
     best_subspace = False
 
     # compute correlation function Q between residual and every subspace in h_subspaces
     for i, inds in enumerate(subspace_inds):
+        if inds_rejected[i]:
+            continue
         window = shifted_windows[inds[0]]
         signals = unwindowed_subspaces[inds[1]]
         subspace = get_subspace(window, signals)
@@ -102,30 +106,45 @@ for it in range(5):
             best_subspace = subspace
         scores[i] = score
 
+    # for all scores that are super low, set their subspaces to be ignored
+    threshold_score = np.quantile(scores[scores > 0], 0.25)
+    inds_rejected[scores < threshold_score] = 1
+
     # once the most-correlated subspace has been found, compute projection onto that subspace
     select_subspace = best_subspace
     new_atom = np.zeros(npts)
 
     for mbr in select_subspace:
-        new_atom = new_atom + (np.dot(mbr, resid) * mbr)
+        factor = np.dot(mbr, resid)
+        new_atom = new_atom + (factor * mbr)
+        print(factor)
 
     found_atoms.append(new_atom)
     reconstruction = reconstruction + new_atom
     new_resid = resid - new_atom
+    resid_norm = np.linalg.norm(new_resid)
 
-    print('iter {}, resid norm {}'.format(it, np.linalg.norm(new_resid)))
+    print('iter {}, resid norm {}, excluded {}'.format(it, resid_norm, sum(inds_rejected)))
 
     resid = new_resid
+
+    if resid_norm < resid_eps:
+        print('ending - residual below target norm')
+        break
 
 #result = omp.omp(X, y, nonneg=False, maxit=50, orthog=False)
 #print('Solution: %r' % result.coef)
 
 plt.clf()
-plt.subplot(211)
+plt.subplot(221)
 plt.plot(domain, y)
-plt.subplot(212)
+plt.subplot(222)
 plt.plot(domain, reconstruction)
 plt.plot(domain, resid, c='red')
+plt.subplot(223)
+plt.specgram(y, NFFT=256, Fs=sr)
+plt.subplot(224)
+plt.specgram(reconstruction, NFFT=256, Fs=sr)
 plt.show()
 
 # generate a dictionary and manufactured solution
